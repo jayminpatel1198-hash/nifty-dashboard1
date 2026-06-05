3d246f0e75c92733ca5ef3e7243743745f85c067
jayminpatel1198-hash <jayminpatel1198@gmail.com>
2026-06-05 14:49:45 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,11 @@
-print("hello")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    token = os.environ.get("UPSTOX_TOKEN", "NO TOKEN")
+    if token == "NO TOKEN":
+        return "<h1>Token not found</h1>"
+    return "<h1>Nifty Dashboard Cloud Running</h1><p>Token loaded: YES</p>"
